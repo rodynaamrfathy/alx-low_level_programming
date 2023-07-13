ebe5050689c5ca14e18639900fcea15b50d0946a
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-13 16:07:03 -0700

    ‘hi’

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * A C program prints with put
+ *
+ * Return: Always 0 (Success)
+ * */
+int main(void)
+{
+    printf("Programming is like building a multilingual puzzle\n);
+    return 0;
+}
