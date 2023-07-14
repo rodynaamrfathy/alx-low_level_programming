85007be00e659bc6fe0599c6414f3e106c1a2175
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-14 14:26:38 -0700

    ‘hi’

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Enter point
+ *
+ * Description: Prints numbers from '0' to '9' .
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
