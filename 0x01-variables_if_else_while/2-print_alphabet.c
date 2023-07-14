1aa2c8857c8a42292888492a9e4434b3201b8b31
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-14 13:37:14 -0700

    ‘hi’

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -15,10 +15,11 @@ int main(void)
 	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
 	int i = 0;
 
-	while(i != \0)
+	while(alpha[i] != '\0')
 	{
 		putchar(alpha[i]);
 		putchar('\n');
+		i++;
 	}
 
 return (0);
