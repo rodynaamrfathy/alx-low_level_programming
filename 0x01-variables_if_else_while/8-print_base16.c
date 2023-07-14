44ca498bd0a66d0e00c77b08e5277c60b905d2bf
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-14 15:47:09 -0700

    ‘hi’

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Enter point
+ *
+ * Description:
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+	}
+
+	while(ch <= 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
