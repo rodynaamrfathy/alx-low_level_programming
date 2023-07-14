9b4706b8aab70b8646a4d6d61c3c48e53f751823
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-13 18:27:36 -0700

    ‘hi’

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description:
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char str[]="and that piece of art is useful" - Dora Korpar, 2015-10-19,";
+	int i=0;
+	while(str[i] != '\0')
+	{
+		putchar(str[i])
+		i++;
+	}
+}
