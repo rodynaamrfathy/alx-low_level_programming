4fa5e5aa9a02f53638d6de33d9cc98a9d047a884
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-14 15:57:39 -0700

    ‘hi’

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Enter point
+ *
+ * Description: A program that prints all possible
+ *              combinations of single-digit numbers with ','.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i = '0';
+	
+	while (i < '10')
+	{
+		putchar(i);
+		if (i != '9')
+			putchar(',');
+		i++;
+	}
+	
+	return (0);
+}
