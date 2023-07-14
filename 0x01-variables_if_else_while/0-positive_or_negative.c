909b7f8a2986459b36f3b0a2f53737aa5f1eb9ba
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-14 09:41:16 -0700

    ‘hi’

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 *
 * main - Entry point
 *
-* Description: n is a random number the program determines weather n is positive or zero or negative
+* Description: Randomly generates a number 'n' and determines whether it is positive, zero, or negative.
 *
 * Return: 0 (Success)
 */
@@ -15,11 +15,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n>0)
-		printf(“%d is positive”,n)
-	else if (n=0)
-		printf(“%d is zero”,n)
+		printf(“%d is positive”,n);
+	else if (n==0)
+		printf(“%d is zero”,n);
 	else if (n<0)
-		printf(“%d is negative”,n)
+		printf(“%d is negative”,n);
+
 	return (0);
 }
