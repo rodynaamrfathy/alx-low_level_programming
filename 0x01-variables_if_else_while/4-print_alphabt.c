e15cf4d8fcebbd4a52c2ef590d8df0bd3a44decb
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-14 14:19:57 -0700

    ‘hi’

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,29 +2,29 @@
 #include <string.h>
 
 /**
- * main - Enter point
+ * main - Entry point
  *
  * Description: Prints the lowercase alphabet letters from 'a' to 'z',
- *              except 'q' and 'e'.  
+ *              except 'q' and 'e'.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        char alpha[] = "abcdefghijklmnopqrstuvwxyz";
-        int i = 0;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
 
-        while (alpha[i] != '\0')
-        {
+	while (alpha[i] != '\0')
+	{
 		if (alpha[i] == 'q')
 			continue;
 		if (alpha[i] == 'e')
-                        continue;
-                putchar(alpha[i]);
-                i++;
-        }
+			continue;
+		putchar(alpha[i]);
+		i++;
+	}
 
-        putchar('\n');
+	putchar('\n');
 
-return (0);
+	return (0);
 }
