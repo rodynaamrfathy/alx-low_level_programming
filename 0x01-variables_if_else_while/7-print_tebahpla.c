15d2614ecdfaaaa53d3532cdcb179e6cab2ef5c7
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-14 14:49:34 -0700

    ‘hi’

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Enter point
+ *
+ * Description: Prints the lowercase alphabet letters from 'a' to 'z',
+ *              in reverse.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+        int i = 25;
+
+        while (i < 0)
+        {
+                putchar(alpha[i]);
+                i--;
+        }
+
+        putchar('\n');
+
+return (0);
+}
