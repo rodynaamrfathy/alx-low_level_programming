93fe5c51cfd6a0f2ba3f20864b8dcd0e96a2a3b4
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-14 17:01:23 -0700

    ‘hi’

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Enter point
+ *
+ * Description:
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i<= 9)
+	{
+		while (j <= 9)
+		{
+			if(i < j)
+			{
+				putchar(i);
+				putchar(j);
+			}
+			if(i != 8 && j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
