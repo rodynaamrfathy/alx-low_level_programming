787ead2fb1a9b34171a27bcec18448a3630892a1
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-14 17:25:45 -0700

    ‘hi’

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -20,8 +20,11 @@ int main(void)
 			{
 				putchar(i);
 				putchar(j);
-				putchar(',');
-                        	putchar(' ');
+				if (i != 56 && j != 57)
+				}
+					putchar(',');
+					putchar(' ');
+				}
 			}
 			j++;
 		}
