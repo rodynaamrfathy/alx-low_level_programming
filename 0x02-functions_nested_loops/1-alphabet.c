a1809b4a0efad42e9aff1e346419b78538b574a8
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-19 16:26:49 -0700

    ‘hi’

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,6 +2,24 @@
 #include "main.h"
 
 /**
+ * print_alphabet - the function  prints the alphabet, in lowercase, followed
+ * by a new line, using _putchar
+ */
+void print_alphabet(void)
+{
+        char c[] = "abcdefghijklmnopqrstuvwxyz";
+        int i = 0;
+
+        while(c[i] != '\0')
+        {
+                _putchar(c[i]);
+                i++;
+        }
+
+        _putchar('\n');
+}
+
+/**
  * main - Entry Point
  *
  * Description:
