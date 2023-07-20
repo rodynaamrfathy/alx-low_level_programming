c603478c6aad3dc6917432793729a46af52d6b7b
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-19 18:19:54 -0700

    ‘hi’

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include<unistd.h>
+#include"main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase 10 times,
+ * followed by a new line.
+ */
+void print_alphabet_x10(void);
+{
+	char c[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+	int j = 0;
+
+	while (j <= 10)
+	{
+		while (c[i] != '\0')
+		{
+			_putchar(c[i]);
+			i++;
+		}
+		j++;
+	}
+	_putchar('\n');
+}
+
