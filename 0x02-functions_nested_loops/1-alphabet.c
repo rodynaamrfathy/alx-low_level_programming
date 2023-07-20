6e43247c2e8e237121344f8f25c9e23f63dc04f7
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-19 18:17:04 -0700

    ‘hi’

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,21 @@
-#include <unistd.h>
-#include "main.h"
+#include<unistd.h>
+#include"main.h"
 
+/**
+ * print_alphabet - prints the alphabet, in lowercase,
+ * followed by a new line.
+ */
 void print_alphabet(void)
 {
-    char c[] = "abcdefghijklmnopqrstuvwxyz";
-    int i = 0;
+	char c[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
 
-    while (c[i] != '\0')
-    {
-        _putchar(c[i]);
-        i++;
-    }
+	while (c[i] != '\0')
+	{
+		_putchar(c[i]);
+		i++;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
+
