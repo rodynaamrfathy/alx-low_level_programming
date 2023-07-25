435eabc1568f298f6108f627c6ebc5b3c907a04d
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-25 15:12:22 -0700

    ‘hi’

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string. 
+ * _strlen - returns the length of a string.
  * @s: the string.
  *
  * Return: returns the length of the string.
