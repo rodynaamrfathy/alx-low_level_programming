1417edb0e827bf4655b1cc5f8b252593d3bc0b2d
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-25 15:50:38 -0700

    ‘hi’

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev -  prints a string, in reverse, followed by a new line.
+ * @s: sting array
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while ( s[i] != '\0' )
+	{
+		i++;
+	}
+
+	while ( i > 0 )
+	{
+		_putchar(s[i]);
+		i--;
+	}
+}
