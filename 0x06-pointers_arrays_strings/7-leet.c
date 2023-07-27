5f4778f994a5457449b4c988e8a358ea884fd183
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-27 07:30:57 -0700

    ‘hi’

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,21 +8,21 @@
  */
 char *leet(char *str)
 {
-    char leet_chars[] = "AEOTLaeotl";
-    char leet_replacements[] = "4307143071";
-    int i, j;
+	char leet_chars[] = "AEOTLaeotl";
+	char leet_replacements[] = "4307143071";
+	int i, j;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        for (j = 0; leet_chars[j] != '\0'; j++)
-        {
-            if (str[i] == leet_chars[j])
-            {
-                str[i] = leet_replacements[j];
-                break;
-            }
-        }
-    }
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; leet_chars[j] != '\0'; j++)
+		{
+			if (str[i] == leet_chars[j])
+			{
+				str[i] = leet_replacements[j];
+				break;
+			}
+		}
+	}
 
-    return (str);
+	return (str);
 }
