aa975d4fe7643f96beba5dfaa19400923bc1e15f
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-29 09:20:27 -0700

    ‘hi’

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,8 +12,11 @@ void print_most_numbers(void)
 
 	for (i = 48 ; i < 58 ; i++)
 	{
-		if (i != 50 || i != 52)
-			_putchar(i);
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		_putchar(i);
 	}
 
 	_putchar('\n');
