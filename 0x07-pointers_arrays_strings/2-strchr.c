99314b1b97663f97d2f93a3eebce72ac0a1d07bc
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-31 12:41:43 -0700

    ‘hi’

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,20 +11,17 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char **ptr;
 
 	for (i = 0 ; s[i] != '\0' ; i++)
-	{
+	{i
 		if (s[i] == c)
 		{
-			ptr = &s[i];
+			return (&s[i]);
 			break;
 		}
 		else
 		{
-			ptr = NULL;
+			return (NULL);
 		}
 	}
-
-	return (ptr);
 }
