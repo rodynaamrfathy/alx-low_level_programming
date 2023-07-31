1fd154387ffb27c270b4d57d80474d7ebd7defd6
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-31 15:01:40 -0700

    ‘hi’

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,29 +1,64 @@
 #include "main.h"
 
 /**
- * print_diagsums -  a function that prints the sum of the 
- * two diagonals of a square matrix of integers.
+ * print_number - prints an integer using _putchar.
+ * @n: number to be printed.
+ *
+ * Return: nothing.
+ */
+void print_number(int n)
+{
+	char c;
+	int temp = (n < 0) ? -n : n;
+	int reversed = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+
+	if (temp == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (temp != 0)
+	{
+		reversed = reversed * 10 + temp % 10;
+		temp /= 10;
+	}
+
+	while (reversed != 0)
+	{
+		temp = reversed % 10;
+		c = '0' + temp;
+		_putchar(c);
+		reversed /= 10;
+	}
+}
+
+/**
+ * print_diagsums -  prints the sum of the two diagonals
+ * of a square matrix of integers.
  * @a: matrix.
- * @size: it's size.
- * 
+ * @size: its size.
+ *
  * Return: nothing
  */
 void print_diagsums(int *a, int size)
 {
-	int i, sum;
+	int i, sum1 = 0, sum2 = 0;
 
-	for (i = 0 ; i < size ; i++)
+	for (i = 0; i < size; i++)
 	{
-		sum += a[i][i]
+		sum1 += a[(i * size) + i];
+		sum2 += a[(i * size) + (size - 1 - i)];
 	}
 
-	i = 0;
-	size--;
-
-	for (i = 0 ; i = size ; i++)
-	{
-		sum += a[i][size];
-		sum += a[size][i];
-		size--;
-	}
+	print_number(sum1);
+	_putchar(',');
+	_putchar(' ');
+	print_number(sum2);
+	_putchar('\n');
 }
