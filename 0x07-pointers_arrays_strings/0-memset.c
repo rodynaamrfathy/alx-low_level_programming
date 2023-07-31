3df373aad0e7ba7050ebbad8578c96807c2330fc
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-31 11:38:44 -0700

    ‘hi’

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,8 +2,8 @@
 
 /**
  * _memset -  a function that fills memory with a constant byte.
- * @s: type pointer to a char,
- * @b: type char, 
+ * @s: type pointer to a char.
+ * @b: type char.
  * @n: unsigned int, the memory area pointed to by s.
  *
  * Return: returns a pointer to the memory area s.
