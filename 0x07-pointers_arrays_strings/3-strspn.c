4646e347950ce31cf937d9955cf3e66365972198
rodynaamrfathy <rodynaamr@icloud.com>
2023-07-31 12:37:19 -0700

    ‘hi’

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strspn - a function that gets the length of a prefix substring.
+ * @s:
+ * @accept:
+ *
+ * Return: returns the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j;
+
+	for(j = 0 ; j < i ; j++)
+	{
+		ptr[j] = accept[j];
+	}
+
+	return (ptr);
+}
