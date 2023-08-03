32c288da23110339ecccf9c543683e8922522dbe
rodynaamrfathy <rodynaamr@icloud.com>
2023-08-03 13:31:00 -0700

    ‘hi’

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,28 +9,28 @@
  */
 int wildcmp_helper(char *s1, char *s2)
 {
-    if (*s2 == '*')
-    {
-        if (*(s2 + 1) == '*')
-            return wildcmp_helper(s1, s2 + 1);
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return wildcmp_helper(s1, s2 + 1);
 
-        if (*s1 == '\0')
-            return wildcmp_helper(s1, s2 + 1);
+		if (*s1 == '\0')
+			return wildcmp_helper(s1, s2 + 1);
 
-        if (wildcmp_helper(s1 + 1, s2))
-            return 1;
+		if (wildcmp_helper(s1 + 1, s2))
+			return 1;
 
-        return wildcmp_helper(s1, s2 + 1);
-    }
+		return wildcmp_helper(s1, s2 + 1);
+	}
 
-    if (*s1 == *s2)
-    {
-        if (*s1 == '\0')
-            return 1;
-        return wildcmp_helper(s1 + 1, s2 + 1);
-    }
+	if (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return 1;
+		return wildcmp_helper(s1 + 1, s2 + 1);
+	}
 
-    return 0;
+	return 0;
 }
 
 /**
@@ -42,5 +42,5 @@ int wildcmp_helper(char *s1, char *s2)
  */
 int wildcmp(char *s1, char *s2)
 {
-    return wildcmp_helper(s1, s2);
+	return wildcmp_helper(s1, s2);
 }
