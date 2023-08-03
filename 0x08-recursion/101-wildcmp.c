fedd0bae2fb177160929c8ed01b2c8005929649e
rodynaamrfathy <rodynaamr@icloud.com>
2023-08-03 13:33:14 -0700

    ‘hi’

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * wildcmp_helper - compares two strings and returns 1 if they can be considered identical
+ * wildcmp_helper - compares two strings
+ * and returns 1 if they can be considered identical
  * @s1: first string
  * @s2: second string with special character *
  *
@@ -12,25 +13,25 @@ int wildcmp_helper(char *s1, char *s2)
 	if (*s2 == '*')
 	{
 		if (*(s2 + 1) == '*')
-			return wildcmp_helper(s1, s2 + 1);
+			return (wildcmp_helper(s1, s2 + 1));
 
 		if (*s1 == '\0')
-			return wildcmp_helper(s1, s2 + 1);
+			return (wildcmp_helper(s1, s2 + 1));
 
 		if (wildcmp_helper(s1 + 1, s2))
-			return 1;
+			return (1);
 
-		return wildcmp_helper(s1, s2 + 1);
+		return (wildcmp_helper(s1, s2 + 1));
 	}
 
 	if (*s1 == *s2)
 	{
 		if (*s1 == '\0')
-			return 1;
+			return (1);
 		return wildcmp_helper(s1 + 1, s2 + 1);
 	}
 
-	return 0;
+	return (0);
 }
 
 /**
@@ -42,5 +43,5 @@ int wildcmp_helper(char *s1, char *s2)
  */
 int wildcmp(char *s1, char *s2)
 {
-	return wildcmp_helper(s1, s2);
+	return (wildcmp_helper(s1, s2));
 }
