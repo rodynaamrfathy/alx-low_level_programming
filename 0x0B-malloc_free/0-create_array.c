381415b7c093db148156be523f78aa8f470fa13a
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-08 07:20:26 -0700

    ‘hi’

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,14 +1,27 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * create_array -  a function that creates an array of chars,
  * and initializes it with a specific char. 
  * @size: array size
- * @c:
+ * @c: char.
  *
  * Return:a pointer to the array, or NULL if it fails.
  */
 char *create_array(unsigned int size, char c)
 {
+	char *s = malloc(sizeof(char) * size);
 
+	if (size == 0 || s == 0)
+	{
+		return (0);
+	}
+	
+	while (size--)
+	{
+		s[size] = c;
+	}
+
+	return (s);
 }
