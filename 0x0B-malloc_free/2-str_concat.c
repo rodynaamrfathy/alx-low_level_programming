40cbab728b57028e6559f4f2e4c5c02082ba83a0
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-08 10:20:21 -0700

    ‘hi’

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,48 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
+ * str_concat - a function that concatenates two strings.
+ * @s1: string one.
+ * @s2: string two.
  *
- *
+ * Return: On success returns a pointer to the concatenates strings.
+ * It returns NULL if insufficient memory was available.
  */
+char *str_concat(char *s1, char *s2)
+{
+	int size1 = 0, size2 = 0, ts = 0, i = 0, j = 0;
+	char *s;
+
+	if (s1 == NULL || s2 == NULL)
+		return (0);
+
+	while (s1[size1] != '\0')
+		size1++;
+
+	while (s2[size2] != '\0')
+		size2++;
+
+	ts = size1 + size2;
+	s = malloc((ts + 1) * sizeof(char));
+
+	if (s == 0)
+		return (NULL);
+
+	while (i < size1)
+	{
+		s[i] = *(s1 + i);
+		i++;
+	}
+
+	while (j < size2)
+	{
+		s[size1 + j] = *(s2 + j);
+		j++;
+	}
+
+	s[ts] = '\0';
+
+	return (s);
+}
