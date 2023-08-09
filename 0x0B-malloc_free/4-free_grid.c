4ad135876a0bf3a1065117e811b41af252dd83a3
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-09 05:30:56 -0700

    ‘hi’

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,1 +1,21 @@
 #include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid -  a function that frees a 2 dimensional grid
+ * previously created by your alloc_grid function.
+ * @grid: prev made 2d array.
+ * @height: fn rows.
+ *
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0 ; i < height ; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
