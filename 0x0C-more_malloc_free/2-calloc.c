371ee6d02e1833b92c1b89b7f6838354013303f0
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-10 10:55:27 -0700

    ‘hi’

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	int *ptr;
+	
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(sizeof(size) * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+
+	for ( ; i < nmemb ; i++)
+	{
+		ptr[i] = 0;
+	}
+
+	return (ptr);
+}
