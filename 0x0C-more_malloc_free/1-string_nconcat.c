0fc41f4c6a6ca1a3d2e7920548c273d97b0f6ef0
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-10 10:25:20 -0700

    ‘hi’

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ptr[i] = s1[i];
 	}
 
-	for (j = 0 ; j < n ; j++)
+	for (j = 0 ; s2[i] != 0 || j < n ; j++)
 	{
 		ptr[i + j] = s2[j];
 	}
