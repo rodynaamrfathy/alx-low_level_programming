6fd26a088a4c7a3e7de031195af8b06ec195725e
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-14 12:18:07 -0700

    ‘hi’

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
-		return NULL;
+		return (NULL);
 	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;
