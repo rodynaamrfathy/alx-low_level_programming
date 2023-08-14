cdcca685d4fabd0a9276e59e73b9efc68dced1b9
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-14 09:49:04 -0700

    ‘hi’

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - a struct contains dog info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description - a struct contains dog info
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
