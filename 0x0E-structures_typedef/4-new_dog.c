df34c76c9909961f64e25a67ba939a2146658a03
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-14 14:39:39 -0700

    ‘hi’

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "dog.h"
 
 /**
@@ -14,19 +15,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
 	new_dog = (dog_t *)malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
-	new_dog->owner = owner;
-
+	new_dog->name = malloc(sizeof(char) * strlen(name) + 1);
+	new_dog->owner = malloc(sizeof(char) * strlen(owner) + 1);
 	if (new_dog->name == NULL || new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog->owner);
 		free(new_dog);
+		return (NULL);
 	}
-
+	strcpy(new_dog->name, name); 
+	strcpy(new_dog->owner, owner);
 	new_dog->age = age;
 
 	return (new_dog);
