80eaf1312fe1f75477d21093e68bd4b84e5fc8ac
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-14 12:25:14 -0700

    ‘hi’

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,8 +18,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 	new_dog->name = name;
-	new_dog->age = age;
 	new_dog->owner = owner;
+
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+	}
+
+	new_dog->age = age;
 
 	return (new_dog);
 }
