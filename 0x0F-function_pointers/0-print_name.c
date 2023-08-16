55763166cd470d5e0678f8728911747167151fbc
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-16 14:34:20 -0700

    ‘hi’

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,23 +4,6 @@
 #include <string.h>
 
 /**
- * _print - print using _putchar()
- * @str: string to be printed.
- *
- * Return: nothing.
- */
-void _print(char *str)
-{
-	int i = 0;
-
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
-}
-
-/**
  * print_name - a function that prints a name.
  * @name: a string , name to be printed
  * @f: pointer to a function whose return type is void and argument is a sting
