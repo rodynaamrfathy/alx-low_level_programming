f296297c56b39d8533e673ce4d86f3549bee5a8a
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-16 15:26:36 -0700

    ‘hi’

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
-	if (array == NULL || size < 0)
+	if (array == NULL)
 		return;
 	for(; i <  size ; i++)
 		action(array[i]);
