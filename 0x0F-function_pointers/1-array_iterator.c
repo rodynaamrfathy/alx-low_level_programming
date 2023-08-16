f12308e430edeaf155c0f3dcf7f8640131927928
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-16 15:23:19 -0700

    ‘hi’

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Write a function that executes a
+ * function given as a parameter on each element of an array.
+ * @array: pointer to an int.
+ * @size:the size of the array
+ * @action:a pointer to the function you need to use.
+ *
+ * Return: nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (array == NULL || size == NULL)
+		return;
+	for(; i <  size ; i++)
+		action(array[i]);
+}
