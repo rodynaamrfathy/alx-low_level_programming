ef5a9522d97125906b2a14442c60686d91834804
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-18 16:59:28 -0700

    ‘hi’

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,5 +17,14 @@ int main(int argc, char *argv[])
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	op_func = get
+	op_func = get_op_func(argv[2]);
+	if(!op_func)
+		printf("Error\n"), exit(99);
+
+	if (argv[2] == "/" || argv[2] == "%" || !b)
+		printf("Error\n"), exit(100);
+
+	printf("%d\n", op_func(a, b));
+
+	return (0);
 }
