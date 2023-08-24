78db672d4161e687bdfdb8bd0ed7ab0e469d2c04
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-24 07:19:02 -0700

    ‘hi’

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,20 @@
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * list_len - a function that returns the number of
- * elements in a linked list_t list.
- * @h: list head.
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
  *
- * Return: umber of elements in a linked.
+ * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
-	size_t count;
+	size_t count = 0;
 
 	while (h)
 	{
 		count++;
 		h = h->next;
 	}
-
 	return (count);
 }
