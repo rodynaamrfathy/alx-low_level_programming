18ecb0e9a1ebafd8e7325fc030406e9e07902e2b
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-24 06:54:23 -0700

    ‘hi’

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,11 +2,11 @@
 #include <string.h>
 
 /**
- * print_list -  a function that prints all the elements of a list_t list.
- * @h: singly linked list.
- *
- * Return: the number of nodes.
- */
+* print_list -  a function that prints all the elements of a list_t list.
+* @h: singly linked list.
+*
+* Return: the number of nodes.
+*/
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
-			 printf("[0] (nil)\n");
+			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		count++;
