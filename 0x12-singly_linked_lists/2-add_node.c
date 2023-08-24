4b2d7f74aac817537bc1aaa4fb2e4a07724d13e8
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-24 08:15:26 -0700

    ‘hi’

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,10 +13,16 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 
 	new_node = malloc(sizeof(list_t));
-	if (!new_node)
+	if (!new_node || !head)
 		return (NULL);
 	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
-	return (head);
+	return (*head);
 }
