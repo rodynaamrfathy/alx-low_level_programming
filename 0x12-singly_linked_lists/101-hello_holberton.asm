045e0cfdbd79eb18b15fbf7344eef67251e65b60
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-24 15:56:24 -0700

    ‘hi’

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,12 @@
-section .data
-	hello db 'Hello, World',0
+section 	.text
+	extern  printf
+	global  main
 
-section .text
-global _start
+main:
 
-_start:
+	mov		edi, msg
+	mov		eax, 0
+	call	printf
 
-	; Write syscall (sys_write)
-	mov rax, 1		; syscall number for sys_write
-	mov rdi, 1		; file descriptor 1 (stdout)
-	lea rsi, [hello]	; adress of the string to print
-	mov rdx, 13		; length of the string
-	syscall
-
-	; Exit syscall (sys_exit)
-	mov rax, 60		; syscall number for sys_exit
-	xor rdi, rdi		; exit status 0
-	syscall
+section		.data
+	hello db 'Hello, World', 0xa, 0
