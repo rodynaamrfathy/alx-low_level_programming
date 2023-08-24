7302ab315a0573aaabd25b425de513b2b348a687
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-24 06:26:52 -0700

    ‘hi’

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 
 /**
  * print_list -  a function that prints all the elements of a list_t list.
@@ -12,7 +13,7 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%d] %s\n", h->str != NULL ? h->len : 0,
+		printf("[%d] %s\n", h->str != NULL ? strlen(h->str) : 0,
 				h->str != NULL ? h->str : "(nil)");
 		count++;
 		h = h->next;
