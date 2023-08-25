49b67ce1db28258c12e5384f34779b17c59479dd
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-25 05:16:13 -0700

    ‘hi’

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,9 @@
-section		.data
-	hello db 'Hello, World', 0xa, 0
-
-section 	.text
-	extern  printf
-	global  main
-
+global    main
+          extern    printf
 main:
-	mov edi, hello		; Load the address of the message
-	call printf		; Call the printf function
-
-	xor eax, eax		; Clear the eax register (return value of printf)
-	ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
