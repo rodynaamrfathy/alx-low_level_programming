5fcea1e6f26aee1578d679d0421c9f1fd68eda8b
rodynaamrfathy <rodynaamr@icloud.com>
2023-08-29 14:27:27 -0700

    ‘hi’

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,14 +2,14 @@
 
 /**
  * print_listint - a function that prints all the elements of a listint_t list.
- * h: head of the singly linked list
+ * @h: head of the singly linked list
  *
  * Return: the number of nodes.
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-	
+
 	if (h)
 	{
 		while (h)
