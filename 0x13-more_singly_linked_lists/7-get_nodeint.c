3192784aea8f16bbf9ded883043a5aa2ea12f752
rodynaamrfathy <rodynaamr@icloud.com>
2023-08-30 01:13:15 -0700

    ‘hi’

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index -  a function that returns the nth
+ * node of a listint_t linked list.
+ * @head: linked list head.
+ * @index: index of node. 
+ *
+ * Return:  nth node of a listint_t linked list,if the node
+ * does not exist, return NULL.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *ptr;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+	ptr = head;
+	for(i = 0; i <= index ; i++)
+	{
+		ptr = ptr->next;
+	}
+	return (ptr);
+}
