a77338ade5a84124fa7f55b256a1b0a2482acf7c
rodynaamrfathy <rodynaamr@icloud.com>
2023-08-30 01:16:33 -0700

    ‘hi’

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,7 +17,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 	ptr = head;
-	for (i = 0; i <= index ; i++)
+	for (i = 0; i < index ; i++)
 	{
 		ptr = ptr->next;
 	}
