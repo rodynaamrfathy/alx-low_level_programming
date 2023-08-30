f4217e80aa544e71711ba4ab8a245932ddff4cc3
rodynaamrfathy <rodynaamr@icloud.com>
2023-08-30 01:41:51 -0700

    ‘hi’

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - a function that returns the sum of all the
+ * data (n) of a listint_t linked list.
+ * @head: head of list.
+ *
+ * Return: returns the sum of all the data (n),
+ * if the list is empty, return 0.
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *ptr;
+	int sum;
+
+	if (!head)
+		return (0);
+	for (ptr = head; ptr; ptr = ptr->next)
+		sum = ptr->n;
+	return (n);
+}
