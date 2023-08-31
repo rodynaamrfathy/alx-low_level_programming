df90140b3a63d918cb00d368a76c32dc12949a90
rodynaamrfathy <rodynaamr@icloud.com>
2023-08-31 07:47:51 -0700

    ‘hi’

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_binary - a function that prints the binary representation of a number.
+ * @n: number in base 10
+ *
+ * Return: nothing.
+ */
+void print_binary(unsigned long int n)
+{
+
+}
