2a2013ec4841774d79bab7c9ab3f756a181ccd6c
rodynaamrfathy <rodynaamr@icloud.com>
2023-09-05 16:29:33 -0700

    ‘hi’

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,11 +3,13 @@
 
 
 /**
- * read_textfile - a function that reads a text file and prints it to the POSIX standard output.
+ * read_textfile - a function that reads a text
+ * file and prints it to the POSIX standard output.
  * @filename: file name.
  * @letters:  number of letters it should read and print.
  *
- * return: NULL return 0, returns the actual number of letters it could read and print.
+ * Return: NULL return 0, returns the actual
+ * number of letters it could read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
