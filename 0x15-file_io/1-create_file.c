d24fd18fc24eff12475cddbd0be2e3e48cd599a9
rodynaamrfathy <rodynaamr@icloud.com>
2023-09-07 05:47:26 -0700

    ‘hi’

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
-		write(2,"fails", 5);
+		write(2, "fails", 5);
 		return (-1);
 	}
 	if (text_content == NULL)
