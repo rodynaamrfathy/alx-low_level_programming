931ccbc6da977fb8a3a07ed1df69b936df4a8bd4
rodynaamrfathy <rodynaamr@icloud.com>
2023-09-07 06:23:17 -0700

    ‘hi’

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,13 +17,8 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd == -1)
-	{
-		write(2, "fails", 5);
-		return (-1);
-	}
-	bytes_written = write(0, text_content, bytes);
-	if (bytes_written == -1)
+	bytes_written = write(fd, text_content, bytes);
+	if (bytes_written == -1 || fd == -1)
 	{
 		close(fd);
 		return (-1);
