8ffbaf53429c100558c3c4ba584369b9839f12f6
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-12 13:31:44 -0700

    ‘hi’

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - a function that counts all the elements of a dlistint_t list.
+ * @h: head of a doubly linked list
+ *
+ * Return: the number of nodes.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0; // Initialize count to 0.
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
