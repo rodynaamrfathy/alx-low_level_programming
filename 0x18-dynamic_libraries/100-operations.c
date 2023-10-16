b6d9bd6cfe9d0950854290f649b745f65b4e37f4
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-16 12:13:21 -0700

    ‘hi’

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * add - substract two numbers
+ * @a: int
+ * @b: int
+ *
+ * Return: a + b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - substract two numbers
+ * @a: int
+ * @b: int
+ *
+ * Return: a - b.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mult - substract two numbers
+ * @a: int
+ * @b: int
+ *
+ * Return: a x b.
+ */
+int mult(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * divison - substract two numbers
+ * @a: int
+ * @b: int
+ *
+ * Return: a / b.
+ */
+int divison(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("not valid\n");
+		exit(98);
+	}
+	return (a / b);
+}
+
+/**
+ * mod - substract two numbers
+ * @a: int
+ * @b: int
+ *
+ * Return: a % b.
+ */
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("not valid\n");
+		exit(98);
+	}
+	return (a % b);
+}
