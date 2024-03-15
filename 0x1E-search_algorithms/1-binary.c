6ebb7189b504ebddcc2f7e043544751183664a06
rodynaamrfathy <rodynaamr@icloud.com>
2024-03-15 12:57:07 -0700

    ‘UPDATE’

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,61 +1,58 @@
 #include "search_algos.h"
+#include <stdio.h>
 
 /**
- * recursive_search - searches for a value in an array of
- * integers using the Binary search algorithm
+ * print_array - prints the elements of an array
  *
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * @array: pointer to the array
+ * @low: starting index of the sub-array
+ * @high: ending index of the sub-array
  */
-int recursive_search(int *array, size_t size, int value)
+void print_array(int *array, int low, int high)
 {
-	size_t half = size / 2;
-	size_t i;
-
-	if (array == NULL || size == 0)
-		return (-1);
-
-	printf("Searching in array");
-
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-
-	printf("\n");
-
-	if (half && size % 2 == 0)
-		half--;
-
-	if (value == array[half])
-		return ((int)half);
-
-	if (value < array[half])
-		return (recursive_search(array, half, value));
-
-	half++;
-
-	return (recursive_search(array + half, size - half, value) + half);
+    int size = high - low + 1;
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
 }
 
 /**
- * binary_search - calls to binary_search to return
- * the index of the number
+ * binary_search - searches for a value in a sorted array using binary search
  *
- * @array: input array
+ * @array: pointer to the sorted array
  * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * @value: value to search for
+ *
+ * Return: index of the value if found, otherwise -1
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int index;
+    if (array == NULL || size == 0)
+    {
+        return (-1);
+    }
 
-	index = recursive_search(array, size, value);
+    int low = 0, high = size - 1;
 
-	if (index >= 0 && array[index] != value)
-		return (-1);
+    while (low <= high)
+    {
+        int mid = (high + low) / 2;
+        print_array(array + low, 0, high - low);
+        if (value == array[mid])
+        {
+            return mid;
+        }
+        else if (value > array[mid])
+        {
+            low = mid + 1;
+        }
+        else if (value < array[mid])
+        {
+            high = mid - 1;
+        }
+    }
 
-	return (index);
+    return (-1);
 }
